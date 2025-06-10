7293cafff25602a29f2680751f4c7835d3ce000d
Mariia <shymanskam524@gmail.com>
2025-06-10 13:36:23 +0200

    update app.js

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+
 import contactsRouter from "./routes/contactsRouter.js";
 
 const app = express();
 
-// Middleware
-app.use(morgan("dev"));
+app.use(morgan("tiny"));
+app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/contacts", contactsRouter);
 
-// 404 для нерозпізнаних маршрутів
-app.use((req, res) => {
-  res.status(404).json({ message: "Not found" });
+app.use((_, res) => {
+  res.status(404).json({ message: "Route not found" });
 });
 
-// Обробник помилок
 app.use((err, req, res, next) => {
   const { status = 500, message = "Server error" } = err;
   res.status(status).json({ message });
 });
 
-export default app;
+app.listen(3000, () => {
+  console.log("Server is running. Use our API on port: 3000");
+});

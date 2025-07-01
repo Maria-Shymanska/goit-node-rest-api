d88e70745b5db7232f2a9bda2c1eb642307cefc0
Mariia <shymanskam524@gmail.com>
2025-07-01 18:48:47 +0200

    add validateFile

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,7 +3,8 @@ import { authSchema, updateSubscriptionSchema } from "../schemas/authSchema.js";
 import validateBody from "../helpers/validateBody.js";
 import authController from "../controllers/authControllers.js";
 import auth from "../helpers/auth.js";
-
+import updload from "../helpers/upload.js";
+import validateFile from "../helpers/validateFile.js";
 const authRouter = express.Router();
 
 authRouter.post(
@@ -20,6 +21,14 @@ authRouter.patch(
   auth,
   validateBody(updateSubscriptionSchema),
   authController.updateUserSubscription
+);
+
+authRouter.patch(
+  "/avatars",
+  auth,
+  updload.single("avatar"),
+  validateFile,
+  authController.updateAvatar
 );
 
 export default authRouter;
